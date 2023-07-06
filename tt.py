457948f02932a037827cee7e7aca51c120b45ec4
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2023-07-06 19:48:31 +0530

    Create tt.py

---
+++ tt.py
@@ -0,0 +1,7 @@
+import request
+ID = 1410223644
+
+url = f"https://api.telegram.org/bot5633216566:AAGVHIaZZIHZ3ge-6ZLDbqsZX0F67szyRDo/sendMessage"
+payload = {"chat_id": ID, "text": "success"}
+
+r = requests.post(url, json=payload)
